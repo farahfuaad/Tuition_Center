2ae9637d3a70be5d6aae72dc342f051de2269b65
farah fuaad <farah.fuaad@gmail.com>
2023-03-06 00:34:05 +0800

    added mode

--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
-public class Main
-{
-    // application code
+public class Main{
 	public static void main(String[] args) {
-
-
         Listofcentres Johor = new Listofcentres(); //the state or area that the centers are in
 		Centres centrename = new Centres();	//the center
 		centrename.setNameofcentre("Pandai Sdn.Bhd");	
 		Johor.getCentres().add(0, centrename); //add center to the state or area that it is in
 
-        //teachers
+        //teacher 1
         Teacher Amin = new Teacher();
 		Names Tname = new Names();
 		Tname.setfstName("Mohd");
 		Tname.setlstName("Amin");
 		Amin.setName(Tname);
 
-        //students in that center
+		//teacher 2
+		Teacher Tina = new Teacher();
+		Names Tname2 = new Names();
+		Tname2.setfstName("Tina");
+		Tname2.setlstName("Turner");
+		Tina.setName(Tname2);
+
+        //students
         Student mike = new Student(); 
 		Names name1 = new Names();
 		name1.setfstName("Michael");
@@ -31,13 +34,26 @@ public class Main
 			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
-	    		
 	    	}
 
-        Student mika = new Student();
+		Student mila = new Student();
 		Names name2 = new Names();
-		name2.setfstName("Mikail");
-		mika.setNames(name2);
+		name2.setfstName("Milani");
+		mila.setNames(name2);
+		try {
+			mila.setMaxlistMark(3);
+			mila.addMarks(98);
+			mila.addMarks(92);
+			mila.addMarks(74);
+		}catch (Exception ex) {
+			System.out.println("Error !!!");
+			ex.printStackTrace();
+		}
+
+        Student mika = new Student();
+		Names name3 = new Names();
+		name3.setfstName("Mikail");
+		mika.setNames(name3);
 		try {
 			mika.setMaxlistMark(3);
 			mika.addMarks(65);
@@ -46,36 +62,60 @@ public class Main
 			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
-	    		
 	    	}
 
-        Student mila = new Student();
-		Names name3 = new Names();
-		name3.setfstName("Milani");
-		mila.setNames(name3);
+		Student ally = new Student();
+		Names name4 = new Names();
+		name4.setfstName("Allya");
+		ally.setNames(name4);
 		try {
-			mila.setMaxlistMark(3);
-			mila.addMarks(98);
-			mila.addMarks(92);
-			mila.addMarks(74);
+			ally.setMaxlistMark(3);
+			ally.addMarks(65);
+			ally.addMarks(74);
+			ally.addMarks(45);
 			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
-	    		
 	    	}
 
         Amin.AddStudent(mike);
 		Amin.AddStudent(mika);
-        Amin.AddStudent(mila);
+		Amin.AddStudent(ally);
+		Tina.AddStudent(mila);
 		centrename.AddTeacher(Amin);
+		centrename.AddTeacher(Tina);
 
-        System.out.println("\nCentre Name: "+Johor.getCentres().get(0).getNameofcentre());
+		
+		Scanner scanner = new Scanner(System.in);
+		//choosing which tutor
+    	
+		System.out.println("\nCentre Name: "+Johor.getCentres().get(0).getNameofcentre());
 		System.out.println("Number of Teachers available: "+centrename.getnumberoftutor());
 		centrename.displaytutors();
 		centrename.displayaveragemarksforcenter();
-		System.out.println("\nTeacher's Name: "+Amin.getname().getfstName()+" "+Amin.getname().getlstName());
-		System.out.println("Number of Students: "+Amin.getnumberofstudents()+"\n");
-		Amin.displaystudents();
-		Amin.displayaverageMarkofStudents();   
+
+		System.out.println("\nEnter the Teacher's No. :");
+
+		String num = scanner.nextLine();
+
+			if (num.equals("1")){
+				System.out.println("\nTeacher's Name: "+Amin.getname().getfstName()+" "+Amin.getname().getlstName());
+				System.out.println("Number of Students: "+Amin.getnumberofstudents()+"\n");
+				Amin.displaystudents();
+				Amin.displayaverageMarkofStudents();
+			}
+			else if(num.equals("2")){
+				System.out.println("\nTeacher's Name: "+Tina.getname().getfstName()+" "+Tina.getname().getlstName());
+				System.out.println("Number of Students: "+Tina.getnumberofstudents()+"\n");
+				Tina.displaystudents();
+				Tina.displayaverageMarkofStudents();
+			}
+			else if(num.equals("3")){
+				System.out.println("-- Error 404 --");
+				return;
+			}
+
+
+           
 	}
 }
