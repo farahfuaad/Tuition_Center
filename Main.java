1a85512d82a39b585a9c0c6701a460e2119aa01a
farah fuaad <farah.fuaad@gmail.com>
2023-03-05 17:54:10 +0800

    edited some ayat

--- Main.java
+++ Main.java
@@ -8,53 +8,54 @@ public class Main
 		centrename.setNameofcentre("Pandai Sdn.Bhd");	
 		Johor.getCentres().add(0, centrename); //add center to the state or area that it is in
 
-        //teacher
+        //teachers
         Teacher Amin = new Teacher();
 		Names Tname = new Names();
 		Tname.setfstName("Mohd");
 		Tname.setlstName("Amin");
 		Amin.setName(Tname);
 
-        Student mike = new Student(); //student in a center
+        //students in that center
+        Student mike = new Student(); 
 		Names name1 = new Names();
 		name1.setfstName("Michael");
 		mike.setNames(name1);
 		try {
 			mike.setMaxlistMark(3);
-			mike.addMarks(30);
-			mike.addMarks(30);
+			mike.addMarks(60);
 			mike.addMarks(40);
-			}catch (Exception ex) {//catch block.. action be taken in case of Exception
+			mike.addMarks(25);
+			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
 	    		
 	    	}
 
-        Student mika = new Student(); //student in a center
+        Student mika = new Student();
 		Names name2 = new Names();
 		name2.setfstName("Mikail");
 		mika.setNames(name2);
 		try {
 			mika.setMaxlistMark(3);
-			mika.addMarks(30);
-			mika.addMarks(30);
-			mika.addMarks(40);
-			}catch (Exception ex) {//catch block.. action be taken in case of Exception
+			mika.addMarks(65);
+			mika.addMarks(100);
+			mika.addMarks(82);
+			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
 	    		
 	    	}
 
-        Student mila = new Student(); //student in a center
+        Student mila = new Student();
 		Names name3 = new Names();
 		name3.setfstName("Milani");
 		mila.setNames(name3);
 		try {
 			mila.setMaxlistMark(3);
-			mila.addMarks(30);
-			mila.addMarks(30);
-			mila.addMarks(40);
-			}catch (Exception ex) {//catch block.. action be taken in case of Exception
+			mila.addMarks(98);
+			mila.addMarks(92);
+			mila.addMarks(74);
+			}catch (Exception ex) {
 	    		System.out.println("Error !!!");
 	    		ex.printStackTrace();
 	    		
@@ -65,11 +66,11 @@ public class Main
         Amin.AddStudent(mila);
 		centrename.AddTeacher(Amin);
 
-        System.out.println("Name of center: "+Johor.getCentres().get(0).getNameofcentre()); // display name of tuition center
+        System.out.println("Name of center: "+Johor.getCentres().get(0).getNameofcentre());
 		System.out.println("Number of tutor: "+centrename.getnumberoftutor());
 		centrename.displaytutors();
 		centrename.displayaveragemarksforcenter();
-		System.out.println("\nTutor: "+Amin.getname().getfstName()+" "+Amin.getname().getlstName());
+		System.out.println("\nTeacher's Name: "+Amin.getname().getfstName()+" "+Amin.getname().getlstName());
 		System.out.println("Number of students: "+Amin.getnumberofstudents()+"\n");
 		Amin.displaystudents();
 		Amin.displayaverageMarkofStudents();   
